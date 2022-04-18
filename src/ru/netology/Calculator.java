30ddaa3e1050eb44a7e6262a2146e5d9c3fe0c01
Anton Komrachkov <a.komrachkov@gmail.com>
2022-04-18 23:59:26 +0300

    Fixed devide by zero

--- src/ru/netology/Calculator.java
+++ src/ru/netology/Calculator.java
@@ -8,7 +8,10 @@ public class Calculator {
     public BinaryOperator<Integer> plus = (x, y) -> x + y;
     public BinaryOperator<Integer> minus = (x, y) -> x - y;
     public BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    public BinaryOperator<Integer> devide = (x, y) -> x / y;
+    //public BinaryOperator<Integer> devide = (x, y) -> x / y; - Версия кода из описания задачи, при y = 0, ошибка, на ноль делить нельзя
+    public BinaryOperator<Integer> devide = (x, y) -> y == 0 ? 0 : x / y;
+    // При равенстве делителя 0, выводим ноль, что, конечно не совсем верно,
+    // но как в тернарном операторе реализовать другое (выдачу исключения, например), не ясно
 
     public UnaryOperator<Integer> pow = x -> x * x;
     public UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
